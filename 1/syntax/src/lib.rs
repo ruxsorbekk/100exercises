ef27963f8e726b6279ec461be05c933918e724e9
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-05-21 17:03:01 +0500

    1✅

---
+++ 1/syntax/src/lib.rs
@@ -0,0 +1,13 @@
+fn compute(a: u32, b: u32) -> u32 {
+    a + b * 2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::compute;
+    
+    #[test]
+    fn case() {
+        assert_eq!(compute(1, 2), 5);
+    }
+}
---
+++ 1/welcome/src/lib.rs
@@ -0,0 +1,13 @@
+fn greeting() -> &'static str {
+    "I'm ready to learn Rust!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::greeting;
+    
+    #[test]
+    fn test_welcome() {
+        assert_eq!(greeting(), "I'm ready to learn Rust!");
+    }
+}

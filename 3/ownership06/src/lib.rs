7726c9dd991a25141449d07e0fe3ecc2dc454297
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-05-27 21:35:01 +0500

    halfway there...

---
+++ 3/ownership06/src/lib.rs
@@ -0,0 +1,52 @@
+pub struct Ticket {
+    title: String, 
+    description: String,
+    status: String,
+}
+
+impl Ticket {
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        Ticket {
+            title, 
+            description,
+            status 
+        }
+    }
+    pub fn title(&self) -> &String {
+        &self.title
+    }
+    pub fn description(&self) -> &String {
+        &self.description
+    }
+    pub fn status(&self) -> &String {
+        &self.status
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Ticket;
+    
+    #[test]
+    fn works() {
+        let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
+        assert_eq!(ticket.title(), "A title");
+        assert_eq!(ticket.description(), "A description");
+        assert_eq!(ticket.status(), "To-Do");
+    }
+}
---
+++ 3/visibility04/src/lib.rs
@@ -0,0 +1,51 @@
+mod ticket {
+    pub struct Ticket {
+        title: String,
+         description: String,
+         status: String,
+    }
+    
+    impl Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
+            if title.is_empty() {
+                panic!("Title cannot be empty");
+            }
+            if title.len() > 50 {
+                panic!("Title cannot be longer than 50 bytes");
+            }
+            if description.is_empty() {
+                panic!("Description cannot be empty");
+            }
+            if description.len() > 500 {
+                panic!("Description cannot be longer than 500 bytes");
+            }
+            if status != "To-Do" && status != "In Progress" && status != "Done" {
+                panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+            }
+            Ticket {
+                title, 
+                description,
+                status }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ticket;
+
+    use super::ticket::Ticket;
+    
+    fn should_not_be_possible() {
+        let ticket = Ticket::new("A little".into(), "A description".into(), "To-Do".into());
+        // assert_eq!(ticket.description, "A description");
+    }
+    
+    // fn encapsulation_cannot_be_violated() {
+    //     let ticket = Ticket {
+    //         title: "A title".into(),
+    //         description: "A description".into(),
+    //         status: "To-Do".into()
+    //     };
+    // }
+}
